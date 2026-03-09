343ea03d162621061a1d5657f55d2764bf92cdc8
Rohit Jangid <rohitjangid1931995@gmail.com>
2026-03-09 18:24:58 +0530

    added deploy scripts

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,5 +1,5 @@
 const { merge } = require("webpack-merge");
-const HTMLWebpackPlugin = require("html-webpack-plugin");
+
 const common = require("./webpack.common");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const packageJsonDeps = require("../package.json").dependencies;
@@ -12,9 +12,6 @@ const devConfig = {
     },
   },
   plugins: [
-    new HTMLWebpackPlugin({
-      template: "./public/index.html",
-    }),
     new ModuleFederationPlugin({
       name: "marketing",
       filename: "remoteEntry.js",
